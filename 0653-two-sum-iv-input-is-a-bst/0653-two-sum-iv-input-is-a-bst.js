1de61b16bb87a6a1659da29e842069ddf4132be7
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-05-17 12:02:27 -0700

    Time: 102 ms (29.03%), Space: 52.2 MB (27.32%) - LeetHub

---
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {boolean}
+ */
+var findTarget = function(root, k) {
+     
+    if (root.left === null && root.right === null) {
+        return false
+    }
+    const numArr = []
+    const stack = [root];
+    
+    while (stack.length) {
+        let current = stack.pop();
+        
+        numArr.push(current.val)
+        
+        if (current.left !== null) stack.push(current.left)
+        if (current.right !== null) stack.push(current.right)
+    }
+    
+    console.log()
+    
+    for (let i = 0; i < numArr.length; i++) {
+        for(let j = i + 1; j < numArr.length; j++) {
+            if (numArr[i] + numArr[j] === k) return true
+        }
+    }
+    
+    return false
+    
+    
+    
+};
