4e31cff13b402b1e0699ef6538c1b4d2f1a6810e
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-05-18 10:53:58 -0700

    Time: 945 ms (87.08%), Space: 30.7 MB (25.78%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.py
+++ 0896-monotonic-array/0896-monotonic-array.py
@@ -11,3 +11,8 @@ class Solution:
                 if nums[idx] > nums[idx+1]: 
                     return False
         return True
+    
+# Rank 195,534
+# Date 5/18/2023
+# Runtime: 949 ms, faster than 84.26%
+# Memory Usage: 30.6 MB, less than 28.55%
