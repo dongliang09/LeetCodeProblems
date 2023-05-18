4b79cc392ec10f2bd0ff6c00fa5918282027d62e
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-05-18 13:45:58 -0700

    Time: 54 ms (13.47%), Space: 16.4 MB (15.70%) - LeetHub

---
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.py
@@ -0,0 +1,54 @@
+class Solution:
+    def tictactoe(self, moves: List[List[int]]) -> str:
+        
+        board = []
+        isPlayerOne = True
+
+        # make a standard 9*9 board with all 0
+        for i in range(3):
+            board.append([0]*3)
+
+        # fill in "X" and "O" into board
+        # first player is 1, second player is 2
+        for i in range(len(moves)):
+            [x,y] = moves[i]
+            if isPlayerOne:
+                board[x][y] = 1
+                isPlayerOne = False
+            else:
+                board[x][y] = 2
+                isPlayerOne = True
+        # print(board)
+        
+        #check who wins
+        left2RightDiag = ""
+        right2LeftDiag = ""
+        cols= ["","",""]
+        for i in range(3):
+            #check rows
+            if ("".join(str(num) for num in board[i]) == "111"):
+                return "A"
+            elif ("".join(str(num) for num in board[i]) == "222"):
+                return "B"
+            
+            #check columns
+            for j in range(3):
+                cols[i] += str(board[j][i])
+                if (cols[i] == "111"):
+                    return "A"
+                elif (cols[i] == "222"):
+                    return "B"
+        
+            #check diagonals
+            left2RightDiag += str(board[i][i])
+            right2LeftDiag += str(board[i][2-i])
+            if (left2RightDiag == "111" or right2LeftDiag == "111"):
+                return "A"
+            if (left2RightDiag == "222" or right2LeftDiag == "222"):
+                return "B"
+            # print (left2RightDiag, right2LeftDiag)
+
+        if len(moves) == 9:  
+            return "Draw"
+        else:
+            return "Pending"
