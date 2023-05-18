d30823031fb05868ce3517ece7cbb00bda985f1c
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-05-18 09:25:32 -0700

    Time: 49 ms (10.81%), Space: 16.2 MB (15.23%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.py
+++ 0709-to-lower-case/0709-to-lower-case.py
@@ -1,3 +1,9 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
         return s.lower()
+    
+    
+# Rank 195,534
+# Date 5/18/2023
+# Runtime: 39 ms, faster than 35.52%
+# Memory Usage: 16.2 MB, less than 30.32%
