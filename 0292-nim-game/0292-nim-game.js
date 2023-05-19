459b6fc111c3bf44f3c5a73d1739aad80d0f0faa
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-05-19 14:41:03 -0700

    Time: 56 ms (62.40%), Space: 42.3 MB (9.92%) - LeetHub

--- 0292-nim-game/0292-nim-game.js
+++ 0292-nim-game/0292-nim-game.js
@@ -7,3 +7,10 @@ var canWinNim = function(n) {
     return n % 4 !== 0
     
 };
+
+/*
+# Rank 195,534
+# Date 5/19/2023
+# Runtime: 60 ms, faster than 39.43%
+# Memory Usage: 41.9 MB, less than 37.08%
+*/
