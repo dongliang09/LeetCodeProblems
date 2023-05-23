2246838638d5db17df859f86e34de58ce81bb790
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-05-22 17:26:39 -0700

    Time: 3654 ms (7.70%), Space: 69.8 MB (6.11%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.js
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.js
@@ -19,6 +19,11 @@ KthLargest.prototype.add = function(val) {
 };
 
 /** 
+# Rank 177,187
+# Date 5/22/2023
+# Runtime:  3663 ms, faster than 7.70%
+# Memory Usage: 69.7 MB, less than 6.56%
+
  * Your KthLargest object will be instantiated and called as such:
  * var obj = new KthLargest(k, nums)
  * var param_1 = obj.add(val)
