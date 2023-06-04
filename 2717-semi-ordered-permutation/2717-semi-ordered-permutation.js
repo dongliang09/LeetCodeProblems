fc0822e35d7a7165c745999a01dcf923aaa2e7a0
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-06-03 23:51:01 -0700

    Time: 87 ms (100.00%), Space: 46.3 MB (100.00%) - LeetHub

--- 2717-semi-ordered-permutation/2717-semi-ordered-permutation.js
+++ 2717-semi-ordered-permutation/2717-semi-ordered-permutation.js
@@ -4,6 +4,14 @@
  */
 var semiOrderedPermutation = function(nums) {
     
+    // goal, 1 reach the front, N reach the end
+    // 
+    // when index of 1 is less than index of N, 
+    // they just swap that many times to the respectively end
+    // 
+    // otherwise, there will be one swap mutual to both 
+    // therefore subtract to the total sum they swap
+    
     if (nums[0] === 1 && nums[nums.length - 1] === nums.length) return 0
     
     let index1 = nums.indexOf(1)
@@ -13,3 +21,10 @@ var semiOrderedPermutation = function(nums) {
     else return index1 + (nums.length - 1 - indexN) - 1
     
 };
+
+/*
+# Rank 163,533
+# Date 6/3/2023
+# Runtime: 90 ms, faster than 100.00%
+# Memory Usage: 46 MB, less than 100.00% 
+*/
