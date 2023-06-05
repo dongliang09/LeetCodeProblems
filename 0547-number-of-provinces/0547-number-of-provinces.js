81df1a66b2fc9402ec9bfc29ba034edc401a8304
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-06-05 00:20:53 -0700

    Time: 66 ms (77.33%), Space: 44.8 MB (63.36%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.js
+++ 0547-number-of-provinces/0547-number-of-provinces.js
@@ -4,17 +4,22 @@
  */
 var findCircleNum = function(isConnected) {
     
+    // ================== idea ==============
     // matrix is mirrored over y=x diagonal, so we only need to check one side of the diagonal 
     
     // if two cities are connected, they will share the same x coordinates or y coordinates
     // each time we can check one row, any connection of 1 means they are connected to the city on that row
     // then check the column on that connection because any city on that column is also connected
     
+    
+    // ==================== global variables ================
+    
     let visited = new Set()
     
     // ===============helper function =============
     
     function checkRow(row) {
+        // return array with any connections we haven't seen and connection is 1 on that row
         let rowConnections = []
         for (let j = row + 1; j < isConnected[0].length; j++) {
             if (isConnected[row][j] === 1 && !visited.has(j)) {
@@ -22,11 +27,11 @@ var findCircleNum = function(isConnected) {
                 visited.add(j)
             }    
         }
-        return rowConnections
-        
+        return rowConnections  
     }
     
     function checkColumn(col) {
+        // return array with any connections we haven't seen and connection is 1 on that column
         let colConnections = []
         for (let i = 0; i < col; i++) {
             if (isConnected[i][col] === 1 && !visited.has(i)) {
@@ -34,8 +39,7 @@ var findCircleNum = function(isConnected) {
                 visited.add(i)
             }    
         }
-        return colConnections
-        
+        return colConnections 
     }
     
     // ==============main function
@@ -46,6 +50,7 @@ var findCircleNum = function(isConnected) {
         
         let stack = [] // stack is used for check if any column has new connection 
         
+        // if we haven't seen the index before, push to stack, then it will check row and colmn
         if (!visited.has(i)) {
             visited.add(i)
             stack.push(i)
@@ -65,3 +70,12 @@ var findCircleNum = function(isConnected) {
     
     return output
 };
+
+
+/*
+# Rank 162,972
+# Date 6/5/2023
+# Runtime: 78 ms, faster than 32.79%
+# Memory Usage: 44.2 MB, less than 87.04% 
+
+*/
