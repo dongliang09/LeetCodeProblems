a57a971ceae62b0345727f859deaf0a20d76a9c9
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-06-13 13:23:33 -0700

    Time: 53 ms (83.47%), Space: 41.7 MB (93.55%) - LeetHub

---
+++ 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var areNumbersAscending = function(s) {
+    
+    
+    //========== idea ================
+    // split the string by space, iterate each element
+    // check if it is number with Number(element)
+    // if it is a number between 2 and 100, push into an array
+    // if the array has more than one element, check if it is greater than the previous one
+    // return false if it is not
+    
+    //=========== codes ==============
+    let numArr = []
+    let words = s. split(" ")
+    
+    for (let i =0; i < words.length;i++) {
+        let curr = Number(words[i])
+        if (2 <= curr && curr <=100) {
+            numArr.push(curr)
+            if (numArr.length !== 1) {
+               if (numArr[numArr.length -1] <= numArr[numArr.length -2]) return false
+            }
+        }
+    }
+    
+    return true
+};
+
+/*
+Rank 156,066
+Date: 06/14/2023
+Runtime: 88 ms
+Memory Usage: 41.8 MB
+*/
