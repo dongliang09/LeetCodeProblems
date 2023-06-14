8a7bb949360ab95b78c60e40bff16b51cfe94c59
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-06-14 16:51:40 -0700

    Time: 82 ms (78.26%), Space: 49.8 MB (65.22%) - LeetHub

--- 1410-html-entity-parser/1410-html-entity-parser.js
+++ 1410-html-entity-parser/1410-html-entity-parser.js
@@ -9,12 +9,19 @@ var entityParser = function(text) {
     text = text.replaceAll("&lt;", "<")
     text = text.replaceAll("&frasl;", "/")
     
+    // have to put this one last, because it could cause combination
     text = text.replaceAll("&amp;", "&")
     
     return text
 };
 
 /*
+
+Rank 154,629
+Date: 06/14/2023
+Runtime: 98 ms, faster than 34.78%  
+Memory Usage: 49.7 MB, less than 65.22% 
+
 =============== test case ==========
 
 "&amp;gt;"
