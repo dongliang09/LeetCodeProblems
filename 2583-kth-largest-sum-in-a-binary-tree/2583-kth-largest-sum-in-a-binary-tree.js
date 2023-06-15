5d7ef5b502090dd5dde9115c1478fefa0cdf9f53
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-06-14 20:31:37 -0700

    Time: 386 ms (66.67%), Space: 120.8 MB (16.67%) - LeetHub

---
+++ 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.js
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthLargestLevelSum = function(root, k) {
+    
+    // =============== idea ===================
+    // use an object to store the sum of each level
+    // sort the object.entries based on sum
+       
+    // =============== code ===================
+    
+    let levelSum = {}
+    root.level = 1
+    
+    let stack = [root]
+    
+    while (stack.length) {
+        let current = stack.pop()
+        
+        // add to sum
+        if (!levelSum[current.level]) levelSum[current.level] = current.val
+        else levelSum[current.level] += current.val
+        
+        //push to stack
+        if (current.left !== null) {
+            current.left.level = current.level + 1
+            stack.push(current.left)
+        }
+        if (current.right !== null) {
+            current.right.level = current.level + 1
+            stack.push(current.right)
+        }
+        
+    }
+    
+    // sort by sum
+    let levelSumEntry = Object.entries(levelSum).sort((a,b)=>Number(b[1])-Number(a[1]))
+    
+    if(!levelSumEntry[k-1]) return -1
+    else return levelSumEntry[k-1][1]
+    
+};
+
+/*
+Rank 154,629
+Date: 06/14/2023
+
+*/
