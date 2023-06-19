80ae2e2d3628859844bebe1749bb051fe4e70cc3
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-06-18 18:00:18 -0700

    Time: 67 ms (21.05%), Space: 42.2 MB (39.47%) - LeetHub

--- 1037-valid-boomerang/1037-valid-boomerang.js
+++ 1037-valid-boomerang/1037-valid-boomerang.js
@@ -4,6 +4,8 @@
  */
 var isBoomerang = function(points) {
     
+    // if slope 1 is not same as slope 2, then they are boomerang
+    
     if (String(points[0]) === String(points[1]) || String(points[1]) === String(points[2]) || String(points[0]) === String(points[2])) return false
     
     let slope1 = (points[0][0] - points[1][0]) === 0 ? "vertical" : (points[0][1] - points[1][1]) / (points[0][0] - points[1][0])
@@ -14,6 +16,10 @@ var isBoomerang = function(points) {
 };
 
 /*
+# Rank 145,290
+# Date 06/18/2023
+# Runtime:  48 ms, faster than 93.42%
+# Memory Usage:  41.9 MB, less than 72.37%
 ============ test case =================
 [[0,0],[1,1],[1,1]]
 */
