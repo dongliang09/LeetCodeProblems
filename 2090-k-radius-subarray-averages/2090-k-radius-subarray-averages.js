c82721e60bf136417ef934e0d59eda49bc8dd3eb
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-06-19 22:26:02 -0700

    Time: 358 ms (80.98%), Space: 89.2 MB (33.74%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.js
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.js
@@ -5,10 +5,19 @@
  */
 var getAverages = function(nums, k) {
     
+    //============ idea ==============
+    // have output array to have the same length as nums aray, and fill it with -1
+    // only update the applicable ones
+    // we will first add the ones in first 2K + 1 elements, then slide the window
+    // each time we slide, we subtract the head, and add the next element
+    
+    //============= code ===========
+    
     let output = new Array(nums.length).fill(-1)
     let currWindowSum = 0
     let calAvgLen = 2 * k +1
     
+    //edge case
     if ( nums.length < k * 2 + 1 ) return output 
     if (k === 0) return nums
         
@@ -29,6 +38,12 @@ var getAverages = function(nums, k) {
 
 
 /*
+
+# Rank 144,707
+# Date 06/19/2023
+# Runtime: 357 ms, faster than 80.98%
+# Memory Usage: 88.8 MB, less than 37.42%
+
 ========== test case ==============
 
 [40527,53696,10730,66491,62141,83909,78635,18560]
