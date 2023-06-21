c7da4c1f88ca4ca7a70d103a9eb262b8f6a4381d
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-06-20 18:56:52 -0700

    Time: 106 ms (7.92%), Space: 44.3 MB (43.44%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.js
+++ 0441-arranging-coins/0441-arranging-coins.js
@@ -4,6 +4,13 @@
  */
 var arrangeCoins = function(n) {
     
+    // ============ idead ========
+    // it is using the formula to calculate the area of trapozoid 
+    // area = (top + bottom) * height / 2
+    // in this case top is 1, area is n, height is what we are looking for
+    
+    // ============= code =========
+    
     let formulaSum = 0
     
     if (n === 1) return 1
@@ -14,3 +21,10 @@ var arrangeCoins = function(n) {
     }
     
 };
+
+/*
+# Rank 143,404
+# Date 06/20/2023
+# Runtime: 104 ms, faster than 9.73%
+# Memory Usage: 44.4 MB, less than 24.89%
+*/
