d8d6f839441cda8918e776217e24a70ca8d52930
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-06-24 18:19:36 -0700

    Time: 83 ms (95.73%), Space: 44.4 MB (85.68%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.js
+++ 0455-assign-cookies/0455-assign-cookies.js
@@ -5,10 +5,18 @@
  */
 var findContentChildren = function(g, s) {
     
+    // =========== idea =========
+    
+    // reverse the array of child and cookie,
+    // since the goal is to give the cookie to children as much as poiisble
+    // give the biggest size cookie to the satisfy child 
+    
+    // =========== code =========
     let output = 0
-    let bound = g.length < s.length ? g.length : s.length
+
     g.sort((a,b)=>b-a)
     s.sort((a,b)=>b-a)
+    
     let sIndex = 0
     
     for (let i = 0; i < g.length; i++) {
@@ -23,6 +31,12 @@ var findContentChildren = function(g, s) {
 };
 
 /*
+
+# Rank 142,221
+# Date 06/22/2023
+# 88 ms, faster than 79.40%
+# Memory Usage: 45.1 MB, less than 35.18%
+
 ========== test case =========
 
 [10,9,8,7]
