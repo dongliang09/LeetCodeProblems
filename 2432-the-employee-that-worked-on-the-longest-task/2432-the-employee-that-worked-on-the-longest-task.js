a5885653c71621dc3392cefa8b62396747e59ac7
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-07-03 13:04:18 -0700

    Time: 82 ms (40.54%), Space: 47 MB (64.86%) - LeetHub

--- 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.js
+++ 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.js
@@ -5,6 +5,15 @@
  */
 var hardestWorker = function(n, logs) {
     
+    // =========== idea ============
+    // take the first logs as default
+    // loop through the logs array, check if the difference is greater than max,
+    // it if is, take the id to update output
+    // if it is the same as max, compare the id,
+    // if id is smaller, update the output
+    
+    //============ code ============
+    
     let maxTime = logs[0][1]
     let output = logs[0][0]
     
@@ -21,7 +30,10 @@ var hardestWorker = function(n, logs) {
 };
 
 /*
-
+# Rank 135,450
+# Date 07/03/2023
+# Runtime: 84 ms, faster than 35.14%
+# Memory Usage: 46.1 MB, less than 97.30%
 ========= test case ==========
 70
 [[36,3],[1,5],[12,8],[25,9],[53,11],[29,12],[52,14]]
