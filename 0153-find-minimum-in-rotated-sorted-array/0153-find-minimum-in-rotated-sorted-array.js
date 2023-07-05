abcd447c8dd87435eaad546cdee9110013d29a9e
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-07-05 13:02:29 -0700

    Time: 59 ms (58.51%), Space: 41.8 MB (70.49%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -4,6 +4,14 @@
  */
 var findMin = function(nums) {
     
+    // ============ idea =========
+    // since the given array is sorted and rotated
+    // so that if first number of array is less than last number of array, 
+    // that mean, the array is not rotated, so we can just return the first number
+    // otherwise, loop through the array, if current element is greater than the next element, 
+    // then we know it is the first element before it rotates
+    
+    // =========== code ==========
     if (nums[0] < nums[nums.length - 1]) return nums[0]
     else {
         for (let i = 0; i < nums.length - 1; i++) {
@@ -13,3 +21,10 @@ var findMin = function(nums) {
     
     return nums[0]
 };
+
+/*
+# Rank 131,689
+# Date 07/05/2023
+# Runtime: 59 ms, faster than 58.51%
+# Memory Usage: 42.2 MB, less than 29.07%
+*/
