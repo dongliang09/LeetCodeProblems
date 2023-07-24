077a90979c0047b5d21aab35326fea822252ba59
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-07-24 09:24:18 -0700

    Time: 7845 ms (5.02%), Space: 43.8 MB (6.11%) - LeetHub

--- 0050-powx-n/0050-powx-n.js
+++ 0050-powx-n/0050-powx-n.js
@@ -5,6 +5,37 @@
  */
 var myPow = function(x, n) {
     
-    return Math.pow(x, n)
+    // return Math.pow(x, n)
+    
+    let isPositive = true
+    let output = x
+    
+    if (x === 0) return 0
+    else if (x === 1) return 1
+    else if (x === -1) {
+        if (n % 2 === 0) return 1
+        else return -1
+    }
+    
+    if (n < 0) {
+        isPositive = false
+        n = -n
+    } else if (n === 0) return 1
+    
+    for (let i = 1; i < n; i++) {
+       output *= x     
+    }
+    
+    if (!isPositive) {
+        output = 1 / output
+    }
+    return output
     
 };
+
+/*
+======== test case =========
+-1.00000
+2147483647
+
+*/
