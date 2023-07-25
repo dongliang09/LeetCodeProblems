9b9762b3770fa6bdb0ee64e261d8fcfe6e4c37b2
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-07-24 17:46:27 -0700

    Time: 78 ms (69.81%), Space: 52.5 MB (58.49%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.ts
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.ts
@@ -9,3 +9,11 @@ function peakIndexInMountainArray(arr: number[]): number {
     
     return output
 };
+
+/*
+
+# Rank 126,722
+# Date 07/24/2023
+# Runtime: 95 ms, faster than 7.55%
+# Memory Usage:  52.6 MB, less than 58.49% 
+*/
