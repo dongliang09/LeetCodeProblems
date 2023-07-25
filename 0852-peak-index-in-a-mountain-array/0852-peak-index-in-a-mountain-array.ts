a85f23d4dabc6d2baae6514c39dc8132a1e69290
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-07-24 17:54:17 -0700

    Time: 96 ms (5.66%), Space: 52.1 MB (92.45%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.ts
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.ts
@@ -1,4 +1,10 @@
 function peakIndexInMountainArray(arr: number[]): number {
+    
+    // ============ idea ===========
+    // since the array is guaranteed to be a mountain array, 
+    // we just need to the the one where mountain[i] > mountain[i+1], where it starts to going downward
+    
+    // ============ code ===========
     let output = 0
     
     for (let i = 0; i < arr.length - 1; i++) {
@@ -14,6 +20,6 @@ function peakIndexInMountainArray(arr: number[]): number {
 
 # Rank 126,722
 # Date 07/24/2023
-# Runtime: 95 ms, faster than 7.55%
+# Runtime: 78 ms, faster than 69.81%
 # Memory Usage:  52.6 MB, less than 58.49% 
 */
