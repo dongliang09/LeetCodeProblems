253c02b3566e9cb23124bca85ab673e9b74e6a13
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-07-26 18:21:56 -0700

    Time: 62 ms (61.54%), Space: 42.1 MB (100.00%) - LeetHub

---
+++ 2299-strong-password-checker-ii/2299-strong-password-checker-ii.ts
@@ -0,0 +1,36 @@
+function strongPasswordCheckerII(password: string): boolean {
+
+    const lowercase = "abcdefghijklmnopqrstuvwxyz"
+    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const digits = "0123456789"
+    const special = "!@#$%^&*()-+"
+    
+    let lowercaseCheck = false
+    let uppercaseCheck = false
+    let digitCheck = false
+    let specialCheck = false
+    let adjCheck = true
+    
+    if (password.length < 8) return false
+    
+    for (let i = 0; i < password.length; i++) {
+        if ( !lowercaseCheck && lowercase.includes(password[i]) ) {
+            lowercaseCheck = true
+        } else if ( !uppercaseCheck && uppercase.includes(password[i]) ) {
+            uppercaseCheck = true
+        } else if ( !digitCheck && digits.includes(password[i]) ) {
+            digitCheck = true
+        } else if ( !specialCheck && special.includes(password[i]) ) {
+            specialCheck = true
+        }
+        
+        if (i !== 0 && password[i] === password[i - 1]) {
+            adjCheck = false
+        }
+    }
+    
+    if (lowercaseCheck && uppercaseCheck && digitCheck && specialCheck && adjCheck)
+        return true
+    else 
+        return false
+};
