ebb38e1cfdb9cf093d68ba93b1ee5c4941839869
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-08-08 13:10:45 -0700

    Time: 49 ms (94.74%), Space: 43.8 MB (77.63%) - LeetHub

--- 0917-reverse-only-letters/0917-reverse-only-letters.ts
+++ 0917-reverse-only-letters/0917-reverse-only-letters.ts
@@ -1,4 +1,17 @@
 function reverseOnlyLetters(s: string): string {
+    
+    
+    // =============== idea ==================
+    // we can have an array to store letters and an array to store the indexes of non-letter characters
+    // and we need to have pointers for each array
+    // after we have all the letters, we can reverse the array
+    // 
+    // Have a for-loop to increment the count 
+    // if loop count is the same as any number in the non-letter array, that means we hits a non-letter character
+    // we can access that with s[non-letter-array[pointer]], then increment the pointer
+    // 
+    // do the same thing simliar to letters
+    // =============== code ==================
 
     let alphabets = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     let letters : string[] = []
@@ -30,3 +43,12 @@ function reverseOnlyLetters(s: string): string {
         return output
     
 };
+
+/*
+
+# Rank 122,268
+# Date 08/08/2023
+# Runtime: 59 ms, faster than 68.42%
+# Memory Usage: 43.7 MB, less than 81.58%
+
+*/
