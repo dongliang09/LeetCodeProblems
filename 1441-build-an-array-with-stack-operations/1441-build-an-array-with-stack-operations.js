e44fb743a9ed021fa1083e7923e6c3ce2a4997e3
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-11-05 15:50:48 -0800

    Time: 50 ms (64.78%), Space: 42 MB (36.55%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.js
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.js
@@ -9,10 +9,8 @@ var buildArray = function(target, n) {
     let output = [];
     
     while (count <= target[target.length-1]) {
-        if (target.indexOf(count) != -1) {
-            output.push("Push");
-        } else {
-            output.push("Push");
+        output.push("Push");
+        if (target.indexOf(count) == -1) {
             output.push("Pop");
         }
         count++;
