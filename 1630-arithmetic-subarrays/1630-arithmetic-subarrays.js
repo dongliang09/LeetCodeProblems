4e7920d7f8195aa6ea898fcd9ed3f86accb5bd31
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-11-22 20:50:02 -0800

    Time: 106 ms (95.15%), Space: 48.1 MB (94.17%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.js
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.js
@@ -12,8 +12,12 @@ var checkArithmeticSubarrays = function(nums, l, r) {
     
     for (let i = 0; i < l.length; i++) {
         isCurrTrue = true;
+        
+        //get each array
         let current = nums.slice(l[i], r[i]+ 1);
         current.sort((a,b)=>(a-b));
+        
+        //check each difference
         diff = current[1] - current[0];
         for (let j = 1; j < current.length; j++) {
             let newDiff = current[j] - current[j-1];
