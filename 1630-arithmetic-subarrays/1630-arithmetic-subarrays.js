af65b5d01551bf9599f980a11dbe7dbd162fa0c3
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-11-22 20:48:54 -0800

    Time: 130 ms (22.33%), Space: 48.3 MB (90.29%) - LeetHub

---
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number[]} l
+ * @param {number[]} r
+ * @return {boolean[]}
+ */
+var checkArithmeticSubarrays = function(nums, l, r) {
+    
+    let diff = 0;
+    let output = [];
+    let isCurrTrue = true;
+    
+    for (let i = 0; i < l.length; i++) {
+        isCurrTrue = true;
+        let current = nums.slice(l[i], r[i]+ 1);
+        current.sort((a,b)=>(a-b));
+        diff = current[1] - current[0];
+        for (let j = 1; j < current.length; j++) {
+            let newDiff = current[j] - current[j-1];
+            if (newDiff != diff) {
+                output.push(false);
+                isCurrTrue = false;
+                break;
+            };
+        }
+        if (isCurrTrue) output.push(true);
+    }
+    
+    return output;
+    
+};
