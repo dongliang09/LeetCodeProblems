ddcd431216d0a42d73285ff7f5814f8d2d186223
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-12-11 09:54:42 -0800

    Time: 75 ms (20.21%), Space: 49.4 MB (13.83%) - LeetHub

---
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var findSpecialInteger = function(arr) {
+    
+    let countObj = {}
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] in countObj) {
+            countObj[arr[i]]++;
+        } else {
+            countObj[arr[i]] = 1;
+        }
+    }
+    
+    let countEntry = Object.entries(countObj);
+
+    for (let i = 0; i < countEntry.length; i++) {
+        if (countEntry[i][1] > arr.length / 4) return countEntry[i][0];
+    }
+    
+    //by default return something
+    return arr[0]  
+};
