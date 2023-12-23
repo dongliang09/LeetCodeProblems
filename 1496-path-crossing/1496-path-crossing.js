b10f77217e2cfe3fd400ce7626773804267eba38
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2023-12-22 19:46:05 -0800

    Time: 59 ms (20.86%), Space: 41.4 MB (99.28%) - LeetHub

---
+++ 1496-path-crossing/1496-path-crossing.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} path
+ * @return {boolean}
+ */
+var isPathCrossing = function(path) {
+    
+    let visited = new Set();
+    let point = [0,0];
+    
+    visited.add(point.toString());
+    
+    for (let i = 0; i < path.length; i++) {
+        if (path[i] === "N") point[1]++;
+        else if (path[i] === "S") point[1]--;
+        else if (path[i] === "W") point[0]--;
+        else if (path[i] === "E") point[0]++;
+        
+        if (visited.has(point.toString())) return true
+        else visited.add(point.toString())
+        
+    }
+    
+    return false
+};
