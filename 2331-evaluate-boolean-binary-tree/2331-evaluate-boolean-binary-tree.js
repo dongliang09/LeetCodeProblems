2be3cc60cadf6b3d95e8d7b4116dd171cdf1f5d4
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2024-01-08 21:29:06 -0800

    Time: 61 ms (69.49%), Space: 48.5 MB (74.58%) - LeetHub

--- 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.js
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.js
@@ -11,19 +11,17 @@
  * @return {boolean}
  */
 var evaluateTree = function(root) {
-    
+    //edge case
     if (root.left === null && root.right === null) return root.val === 0 ? false : true;
     
-    // let isLeftNodeBool = root.left.val === 0 || root.left.val === 1;
-    // let isRightNodeBool = root.right.val === 0 || root.right.val === 1;
-    // if (isLeftNodeBool && isRightNodeBool) {
-    //     // if both leaf are 0 or 1
-    //     if (root.val === 2) return root.left.val + root.right.val;
-    //     else if (root.val === 3) return root.left.val * root.right.val;
-    // } else {
-    //     evaluateTree
-    // }
-    
+    //if current node is AND or OR, call recursively with child node
     if (root.val === 2) return evaluateTree(root.left) + evaluateTree(root.right);
     else if (root.val === 3) return evaluateTree(root.left) * evaluateTree(root.right);
 };
+
+
+/*
+Date: 1/8/2024
+Runtime: 70 ms, faster than 25.99% 
+Memory Usage: 48.3 MB, less than 85.88% 
+*/
