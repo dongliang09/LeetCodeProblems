2b89bcd1137c4c545da9c0031e671429eee1f2b9
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2024-02-18 21:50:37 -0800

    Time: 52 ms (87.00%), Space: 50.7 MB (21.66%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.js
+++ 1528-shuffle-string/1528-shuffle-string.js
@@ -14,3 +14,10 @@ var restoreString = function(s, indices) {
     return arr.join('')
     
 };
+
+/*
+Date: 02/18/2024
+Rank: 120,099
+Runtime: 57 ms, faster than 70.28% 
+Memory Usage: 50.7 MB, less than 27.30%
+*/
