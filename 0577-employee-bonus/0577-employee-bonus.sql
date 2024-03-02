73b92e850fb2ab09a1403d60a23442736d974d77
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2024-03-02 14:18:06 -0800

    Time: 1525 ms (94.64%), Space: 0B (100.00%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -5,3 +5,7 @@ FROM Employee
 LEFT JOIN Bonus
 ON Employee.empId = Bonus.empId
 WHERE Bonus.bonus < 1000 or Bonus.bonus IS NULL;
+
+# Date: 3/2/2024
+# Runtime: 2908 ms, faster than 6.39% 
+# Memory Usage: 0B, less than 100.00%
