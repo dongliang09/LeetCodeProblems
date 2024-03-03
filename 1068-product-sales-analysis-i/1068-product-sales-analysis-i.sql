d472323f296d091bddd5a21873aab9f9171b957e
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2024-03-02 16:07:11 -0800

    Time: 4304 ms (5.00%), Space: 0B (100.00%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -4,3 +4,7 @@ SELECT Product.product_name, Sales.year, Sales.price
 FROM Sales
 JOIN Product
 WHERE Sales.product_id = Product.product_id;
+
+# Date: 3/2/2024
+# Runtime: 2455 ms, faster than 31.31%
+# Memory Usage: 0B, less than 100.00%
