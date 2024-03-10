553c67c884f5e4dac3dc60d66a4ae5f20404a6d1
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2024-03-09 22:25:09 -0800

    Time: 64 ms (52.12%), Space: 51.9 MB (46.97%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.js
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.js
@@ -6,19 +6,19 @@ var intersection = function(nums) {
     
     if (nums.length === 1) return nums[0].sort((a,b)=>a-b);
     
-    let commonSet = new Set()
-    let set1 = new Set(nums[0])
-    let set2 = new Set(nums[1])
-    let intersection = new Set();
+    let commonSet = new Set(nums[0])
+//     let set1 = new Set(nums[0])
+//     let set2 = new Set(nums[1])
+//     let intersection = new Set();
 
-    for (const elem of set2) {
-        if (set1.has(elem)) {
-          intersection.add(elem); 
-        }
-    }
-    commonSet = new Set([...intersection]);
+//     for (const elem of set2) {
+//         if (set1.has(elem)) {
+//           intersection.add(elem); 
+//         }
+//     }
+//     commonSet = new Set([...intersection]);
     
-    for ( let i = 2; i < nums.length; i++) {
+    for ( let i = 1; i < nums.length; i++) {
         let currSet = new Set(nums[i])
         for (const elem of commonSet) {
             if (!(currSet.has(elem))) {
