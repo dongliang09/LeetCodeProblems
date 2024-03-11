0397ff8ea459115b82c78fbf40587fbc225f22f7
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2024-03-11 00:00:42 -0700

    Time: 55 ms (58.57%), Space: 49.1 MB (62.70%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.js
@@ -0,0 +1,43 @@
+/**
+ * @param {string} order
+ * @param {string} s
+ * @return {string}
+ */
+var customSortString = function(order, s) {
+    
+    let freqObj = {};
+    let visited = new Set();
+    let sLetters = [];
+    let outputArr = [];
+    
+    //count frequency
+    for (let i = 0 ; i < s.length; i++) {
+        if (s[i] in freqObj) freqObj[s[i]]++;
+        else freqObj[s[i]] = 1;
+    }
+    
+    //add the letter in order first
+    for ( let i = 0; i < order.length; i++) {
+        let currLetter = order[i];
+        if (currLetter in freqObj) {
+            visited.add(currLetter);
+            for (let j = 0; j < freqObj[currLetter]; j++) {
+                outputArr.push(currLetter)
+            }
+        }
+    }
+    
+    // find other letter not in order, and add them to output array
+    sLetters = Object.keys(freqObj);
+    for (let i = 0; i < sLetters.length; i++) {
+        let currLetter = sLetters[i];
+        if (!visited.has(currLetter)) {
+            for (let j = 0; j < freqObj[currLetter]; j++) {
+                outputArr.push(currLetter)
+            }
+        }
+    }
+    
+    return outputArr.join("");
+    
+};
