a4188da8ab7dbb515192f1b86c592534507c0442
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2024-03-21 18:44:40 -0700

    Time: 143 ms (86.48%), Space: 61.6 MB (86.75%) - LeetHub

---
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.js
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {number} a
+ * @param {number} b
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeInBetween = function(list1, a, b, list2) {
+    
+    let list1Count = 0;
+    let list2Count = 0;
+    let currList1Node;
+    let currNode = list1; 
+    
+    // go through linked list until ath node
+    while (list1Count < a - 1) {
+        currNode = currNode.next;
+        list1Count++;
+    }
+    
+    //make a shallow copy of ath node 
+    currList1Node  = new ListNode(currNode);
+    currList1Node.next = currNode.next;
+
+    // connect to list 2
+    currNode.next = list2;
+    currNode = list2;
+     
+    // go throught all of list 2
+    while (currNode.next !== null) {
+        currNode = currNode.next
+    }
+    
+    // console.log(list1, currList1Node)
+    
+    // find the bth node in list 1
+    while (list1Count < b + 1) {
+        currList1Node = currList1Node.next;
+        list1Count++;
+    }
+    // console.log(currList1Node.val)
+    
+    // connect list 2 to bth node of list 1
+    currNode.next = currList1Node;
+    currNode = currList1Node;
+    
+    // console.log(currNode.val)
+    
+    return list1
+    
+    
+};
