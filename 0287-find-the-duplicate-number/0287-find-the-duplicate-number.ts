5774d121e3a9743943a37f878881b017ac383068
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2024-03-23 18:38:01 -0700

    Time: 81 ms (64.47%), Space: 72.4 MB (23.76%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.ts
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.ts
@@ -1,19 +1,12 @@
 function findDuplicate(nums: number[]): number {
     
-    let output = nums[0];
-    let isFound = false;
+    let visited = new Set();
     
     for (let i = 0; i < nums.length; i++) {
-        let currentNum = nums[i];
-        for (let j = i+1; j < nums.length; j++) {
-            if (nums[j] === currentNum) {
-                output = currentNum;
-                isFound = true;
-                break;
-            } 
-        }  
-        if(isFound) break;
+        if (visited.has(nums[i]))
+            return nums[i];
+        else 
+            visited.add(nums[i]);
     }
-
-    return output;
+    
 };
