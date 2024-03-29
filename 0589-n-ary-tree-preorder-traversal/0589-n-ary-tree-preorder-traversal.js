3867a301270f0553084cb80e8f1ecb3d63f1c0b3
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2024-03-29 12:43:33 -0700

    Time: 74 ms (52.27%), Space: 53 MB (71.97%) - LeetHub

--- 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.js
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.js
@@ -23,6 +23,7 @@ var preorder = function(root) {
         
         output.push(curr.val);
         
+        // preorder needs to reverse the order of children pushed into the stack
         if (curr.children.length > 0) {
             for (let i = curr.children.length- 1; i >= 0; i--) {
                 stack.push(curr.children[i]);
@@ -34,3 +35,9 @@ var preorder = function(root) {
     return output;
     
 };
+
+/*
+Date: 03/24/2024
+Runtime: 72 ms, faster than 62.88% 
+Memory Usage: 52.8 MB, less than 82.20%
+*/
