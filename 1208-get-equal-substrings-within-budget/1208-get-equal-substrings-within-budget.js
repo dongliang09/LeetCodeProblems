55546261599615687004a1545abf1cb42113e8a4
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2024-06-01 09:24:09 -0700

    Time: 56 ms (83.76%), Space: 54.7 MB (9.18%) - LeetHub

---
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.js
@@ -0,0 +1,51 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @param {number} maxCost
+ * @return {number}
+ */
+var equalSubstring = function(s, t, maxCost) {
+    
+    // find the difference between each position
+    // use sliding window to find the max sum and max cost
+    
+    let diffArr = [];
+    let maxLen = 0;
+    let currSum = 0;
+    let right = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        diffArr.push(Math.abs(s.charCodeAt(i) - t.charCodeAt(i)));
+    }
+    
+    if (s.length === 1 && diffArr[0] <= maxCost) return 1
+    if (s === t) return s.length
+    
+    for (let i = 0; i < s.length; i++) {
+        
+        //add the right, add at least one number regardless
+        do {
+          currSum += diffArr[right];
+          right++;
+        } while (  currSum + diffArr[right] <= maxCost )
+         
+        // update max length    
+        if (currSum <= maxCost && right - i > maxLen) maxLen = right - i;
+        
+        //remove left
+        currSum = currSum - diffArr[i]
+    }
+    
+    return maxLen
+};
+
+
+/*
+============== test case =========
+"krrgw"
+"zjxss"
+19
+"ujteygggjwxnfl"
+"nstsenrzttikoy"
+43
+*/
