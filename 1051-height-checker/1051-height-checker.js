20a1e3b134c4f62033b7c58d30dfb98e8530689a
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2024-06-13 17:52:02 -0700

    Time: 54 ms (58.38%), Space: 48.6 MB (87.12%) - LeetHub

--- 1051-height-checker/1051-height-checker.js
+++ 1051-height-checker/1051-height-checker.js
@@ -7,8 +7,7 @@ var heightChecker = function(heights) {
     let expected = heights.slice();
     expected.sort((a,b)=>a-b)
     let output = 0;
-    
-    
+       
     for (let i = 0 ; i < heights.length; i++) {
         if (expected[i] !== heights[i]) output++;
     }
@@ -16,3 +15,9 @@ var heightChecker = function(heights) {
     return output
     
 };
+
+/*
+Date: 06/10/2024
+Runtime: 50 ms, faster than 77.46% 
+Memory Usage: 49.1 MB, less than 40.89% 
+*/
