420f709a27ef03ab32bbf9dae6529b92bf6ac771
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2024-06-18 11:41:52 -0700

    Time: 91 ms (10.27%), Space: 50.9 MB (36.30%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.js
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.js
@@ -8,7 +8,7 @@ var judgeSquareSum = function(c) {
     // b^2 = c - a^2
     // if b is interger, then return true
     
-    if (c=== 0 ) return true
+    if ( c === 0 ) return true
     
     for (let i = 0; i * i < c; i++) {
         if (Math.sqrt(c - i**2) % 1 === 0) return true
@@ -16,3 +16,9 @@ var judgeSquareSum = function(c) {
     
     return false
 };
+
+/*
+Date: 06/18/2024
+Runtime: 65 ms, faster than 32.19% 
+Memory Usage: 50.9 MB, less than 39.04% 
+*/
