e0c963acfd54cfde3484e76854df11afb1909b11
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2024-07-11 18:59:24 -0700

    Time: 162 ms (38.41%), Space: 18.2 MB (72.10%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
@@ -11,3 +11,8 @@ class Solution:
                 strS.append(char)
                 
         return ''.join(strS)
+    
+    # Date: 07/11/20224
+    # Rank: 110,619
+    # Runtime: 141 ms, faster than 53.39% 
+    # Memory Usage: 18.3 MB, less than 17.14% 
