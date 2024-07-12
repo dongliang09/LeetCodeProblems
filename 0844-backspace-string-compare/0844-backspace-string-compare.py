cf64b53dfab3bd3efe6b64d48c8b8fc6ec8621d9
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2024-07-11 18:46:43 -0700

    Time: 34 ms (68.25%), Space: 16.5 MB (23.20%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -0,0 +1,79 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        
+        strS = []
+        strT = []
+        
+        for char in s:
+            if (char == '#'):
+                if(len(strS) != 0):
+                    strS.pop()
+            else:
+                strS.append(char)
+                
+        for char in t:
+            if (char == '#'):
+                if(len(strT) != 0):
+                    strT.pop() 
+            else:
+                strT.append(char)
+        
+        # print(strS, strT)
+        
+        if (''.join(strS) == ''.join(strT)):
+            return True
+        else:
+            return False
+  
+    # =========== first try ==========
+    #         strS = []
+    #         strT = []
+    #         skipLtr = 0
+    #         currIdx = len(s) - 1
+
+    #         while (currIdx >= 0):
+    #             if(skipLtr > 0):
+    #                 if (s[currIdx] == "#"):
+    #                     skipLtr = skipLtr + 1
+    #                 else:
+    #                     currIdx = currIdx - skipLtr + 1
+    #                     skipLtr = 0
+    #             else: 
+    #                 if (s[currIdx] == "#"):
+    #                     skipLtr = skipLtr + 1
+    #                 else:
+    #                     strS.append(s[currIdx])
+
+    #             currIdx -= 1
+
+
+    #         currIdx = len(t) - 1
+    #         skipLtr = 0
+
+    #         while (currIdx >= 0):
+    #             if(skipLtr > 0):
+    #                 if (t[currIdx] == "#"):
+    #                     skipLtr = skipLtr + 1
+    #                 else:
+    #                     currIdx = currIdx - skipLtr + 1
+    #                     skipLtr = 0
+    #             else: 
+    #                 if (t[currIdx] == "#"):
+    #                     skipLtr = skipLtr + 1
+    #                 else:
+    #                     strT.append(t[currIdx])
+
+    #             currIdx -= 1
+
+    #         print(strS, strT)
+
+    #         if (''.join(strS) == ''.join(strT)):
+    #             return True
+    #         else:
+    #             return False
+
+    # ============ test cases =============
+
+    # "bxj##tw"
+    # "bxo#j##tw"
+        
