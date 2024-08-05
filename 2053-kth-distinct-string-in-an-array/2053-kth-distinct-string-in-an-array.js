64d63fdf07ae092441ae7ec6580847ffe7094d60
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2024-08-05 15:50:06 -0700

    Time: 53 ms (95.92%), Space: 52.3 MB (57.14%) - LeetHub

--- 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.js
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.js
@@ -7,17 +7,20 @@ var kthDistinct = function(arr, k) {
     
     let freqObj = {};
     
+    //create object to store the string and frequency
     for (const letter of arr) {
         if(freqObj[letter] === undefined) freqObj[letter] = 1
         else freqObj[letter]++
     }
     
+    //only push to array if frequency is 1, order the array by the order of original array
     let singleArr = []
     
     for (const letter of arr) {
         if (freqObj[letter] === 1) singleArr.push(letter)
     }
     
+    // return value after checking the length of ordered array 
     if (k > singleArr.length) return ""
     return singleArr[k-1]
     
