a3ef4c5c8b4a7b45c29fa816751ec89dd059cb40
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2024-09-01 23:28:41 -0700

    Time: 87 ms (36.14%), Space: 59.5 MB (87.95%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.js
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.js
@@ -8,27 +8,28 @@ var chalkReplacer = function(chalk, k) {
     let sum = 0;
     let output = 0;
     
+    // loop only once to get the sum of array
     for (let i = 0; i < chalk.length; i++) {
-        
         let curr = chalk[i];
         if (k < curr) return i
         k -= curr;
-        sum += curr;
-        
+        sum += curr; 
     }
     
+    // in case, k is multiple of array's total sum
     if (k >= sum) k = k % sum;
     if (k === 0) return 0
     
     for (let i = 0; i < chalk.length; i++) {
-        
         let curr = chalk[i]
         if (k < curr) return i
         k -= curr;
-        
     }
-    
-    
-    
+      
     return output
 };
+
+/*
+Runtime: 72 ms, faster than 84.34% 
+Memory Usage: 59 MB, less than 95.18% 
+*/
