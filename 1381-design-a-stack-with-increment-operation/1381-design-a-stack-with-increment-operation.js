515c8703cd808a0ae5eb93d38a81d09efe5092e0
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2024-09-29 17:12:36 -0700

    Time: 86 ms (95.00%), Space: 58.6 MB (40.00%) - LeetHub

---
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number} maxSize
+ */
+var CustomStack = function(maxSize) {
+    this.data = []
+    this.size = maxSize;
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+CustomStack.prototype.push = function(x) {
+    if (this.data.length < this.size) this.data.push(x)
+};
+
+/**
+ * @return {number}
+ */
+CustomStack.prototype.pop = function() {
+    if (this.data.length > 0) return this.data.pop()
+    else return -1
+};
+
+/** 
+ * @param {number} k 
+ * @param {number} val
+ * @return {void}
+ */
+CustomStack.prototype.increment = function(k, val) {
+    let endPos = k;
+
+    if (endPos > this.data.length) endPos = this.data.length
+    
+    for (let i = 0; i < endPos; i++) {
+        this.data[i] += val
+    }
+};
+
+/** 
+ * Your CustomStack object will be instantiated and called as such:
+ * var obj = new CustomStack(maxSize)
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * obj.increment(k,val)
+ */
