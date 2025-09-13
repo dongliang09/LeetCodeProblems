9ac9c0b9fce339f12afb711b5ca865d3e04e1cea
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2025-09-12 22:54:42 -0700

    Time: 1 ms (47.96%), Space: 56 MB (28.91%) - LeetHub

--- 3674-minimum-operations-to-equalize-array/3674-minimum-operations-to-equalize-array.js
+++ 3674-minimum-operations-to-equalize-array/3674-minimum-operations-to-equalize-array.js
@@ -7,9 +7,16 @@ var minOperations = function(nums) {
 
     
     for (let i = 0; i < nums.length; i++) {
+        if (i !== 0 && !set.has(nums[i])) return 1
         set.add(nums[i]);
     }
 
-    return set.size === 1 ? 0 : 1
+    return 0
 
 };
+
+/**
+Date: 09/12/2025
+Runtime: 2ms beats 11.22%
+Memory 56.10MB beats 28.91%
+ */
