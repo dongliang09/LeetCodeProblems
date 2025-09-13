56d78f2fb7965b44cdeaab035680394317a3048c
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2025-09-12 23:24:51 -0700

    Time: 0 ms (100%), Space: 74 MB (97.88%) - LeetHub

---
+++ 3675-minimum-operations-to-transform-string/3675-minimum-operations-to-transform-string.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minOperations = function(s) {
+    let alphabet = 'bcdefghijklmnopqrstuvwxyz';
+
+    for (let i = 0; i < alphabet.length; i++) {
+        if (s.indexOf(alphabet[i]) !== -1) 
+          return (26 - (alphabet.charCodeAt(i) - 96)) % 26 + 1
+    }
+
+    return 0
+
+};
+
+/**
+=============== first try =============
+// fail on very long string
+
+    let arr = [];
+    let set = new Set();
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] !== 'a') {
+            let letterCode = (26 - (s.charCodeAt(i) - 96)) % 26 + 1;
+            if (arr.indexOf(letterCode === -1)) arr.push(letterCode)
+            set.add(s[i])
+        }
+    }
+
+    if (set.size === 0) return 0
+
+    return Math.max(...arr)
+ */
