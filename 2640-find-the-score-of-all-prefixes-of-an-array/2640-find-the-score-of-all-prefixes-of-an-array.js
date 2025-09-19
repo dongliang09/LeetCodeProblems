362b07aa00fa69aa1cef99e43c51e2c2392aaeea
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2025-09-19 09:15:33 -0700

    Time: 6 ms (50%), Space: 81.7 MB (91.67%) - LeetHub

---
+++ 2640-find-the-score-of-all-prefixes-of-an-array/2640-find-the-score-of-all-prefixes-of-an-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findPrefixScore = function(nums) {
+    let max = nums[0];
+    let sum = 0;
+    let output = []
+
+    for (let i = 0; i < nums.length; i++) {
+        let curr = nums[i];
+        if (curr > max) max = curr;
+        let conversion = curr + max;
+        sum += conversion;
+        output.push(sum)
+    }
+
+    return output
+};
