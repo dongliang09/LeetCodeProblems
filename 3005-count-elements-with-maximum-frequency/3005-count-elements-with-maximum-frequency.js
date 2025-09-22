acc4a0f3ee32cfc78eeb1fa1eb425a5c4a695e9d
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2025-09-21 23:35:54 -0700

    Time: 0 ms (100%), Space: 55.8 MB (71.26%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.js
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.js
@@ -3,33 +3,24 @@
  * @return {number}
  */
 var maxFrequencyElements = function(nums) {
-    
-    let freqObj = {};
-    let freqEntries = [];
-    let maxFreq;
-    let output = 0;
-    
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] in freqObj) freqObj[nums[i]]++;
-        else freqObj[nums[i]] = 1;
+
+    let freqObj = {}
+    let output = 0
+
+    for(let i = 0; i < nums.length; i++) {
+        let curr = nums[i]
+        if (freqObj[curr] === undefined) freqObj[curr] = 1;
+        else freqObj[curr]++
     }
-    
-    freqEntries = Object.entries(freqObj).sort((a,b)=>b[1]-a[1]);
-    
-    maxFreq = freqEntries[0][1];
-    
-    for (let i = 0; i < freqEntries.length; i++) {
-        if (freqEntries[i][1] === maxFreq) output += maxFreq
+
+    let sortArr = Object.entries(freqObj).sort((a,b)=> b[1] - a[1])
+    let maxFreq = sortArr[0][1];
+
+    for (let i = 0; i < sortArr.length; i++) {
+        if (sortArr[i][1] === maxFreq) output += maxFreq
         else break;
     }
     
     return output
     
 };
-
-
-/*
-Date: 3/7/2024
-Runtime: 76 ms, faster than 9.29% 
-Memory Usage: 50.9 MB, less than 46.70%
-*/
