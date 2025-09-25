a5c38eb95948b8a3791653e29112acf795c972a4
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2025-09-25 12:40:35 -0700

    Time: 3 ms (57.07%), Space: 55.5 MB (46.92%) - LeetHub

---
+++ 0120-triangle/0120-triangle.js
@@ -0,0 +1,58 @@
+/**
+ * @param {number[][]} triangle
+ * @return {number}
+ */
+var minimumTotal = function(triangle) {
+
+    // start from the bottom second row, moving up the triangle
+    for (let i = triangle.length - 2; i >= 0; i--) {
+        for (let j = 0; j < triangle[i].length; j++) {
+            let curr = triangle[i][j]
+            triangle[i][j] = Math.min(triangle[i + 1][j] + curr, triangle[i + 1][j + 1] + curr)
+        }
+    }
+
+    return triangle[0][0]
+
+
+};
+
+/**
+===================first try =================
+// fail on testcase where we have big triangle
+
+    let minSum = Infinity;
+    let stack =[{row:0, index: 0, sum: triangle[0][0]}]
+
+    while (stack.length) {
+        let curr = stack.pop();
+        
+        // console.log(curr, minSum)
+        //check sum if it reaches the last row
+        if (curr.row >= triangle.length - 1) {
+            if (curr.sum < minSum) minSum = curr.sum
+        } 
+        
+        // add each index of next row into stack
+        if (curr.row + 1 < triangle.length) {
+            if ( curr.index < triangle[curr.row+ 1].length) {
+                stack.push({
+                    row: curr.row + 1, 
+                    index: curr.index,
+                    sum: curr.sum + triangle[curr.row + 1][curr.index]}
+                )
+            }
+            if ( curr.index + 1 < triangle[curr.row+ 1].length) {
+                stack.push({
+                    row: curr.row + 1, 
+                    index: curr.index + 1,
+                    sum: curr.sum + triangle[curr.row + 1][curr.index + 1]}
+                )
+            }
+            
+        }
+
+    }
+    
+    return minSum
+ */
