8097f7a682c9f26fb039ddddefae70c054cf5c28
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2025-09-30 00:34:10 -0700

    Time: 110 ms (39.17%), Space: 59.4 MB (65.83%) - LeetHub

---
+++ 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var triangularSum = function(nums) {
+    
+    let temp = nums;
+    let temp2 = []
+
+    while (temp.length > 1) {
+        for (let i = 0; i < temp.length - 1; i++) {
+            temp2.push( (temp[i] + temp[i+1]) % 10 )
+        }
+        temp = temp2
+        temp2 = []
+    }
+
+    return temp[0]
+
+};
