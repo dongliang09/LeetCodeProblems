76cac3c9b4f95b2ca7dee42027feb1096bb4bed3
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2025-09-30 23:29:34 -0700

    Time: 0 ms (100%), Space: 53.7 MB (45.87%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} numBottles
+ * @param {number} numExchange
+ * @return {number}
+ */
+var numWaterBottles = function(numBottles, numExchange) {
+
+    let output = numBottles;
+    let empty = numBottles;
+    let leftover = 0
+
+    while (empty >= numExchange) {
+        let exchange =  Math.floor(empty / numExchange)
+        leftover = empty - exchange * numExchange
+        empty = leftover + exchange
+        output += exchange
+    }
+
+    return output
+};
