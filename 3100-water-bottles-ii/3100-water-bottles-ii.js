ab651734267a1e0d163820ac127a7e0e20b58497
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2025-10-01 20:11:02 -0700

    Time: 57 ms (52.94%), Space: 55.6 MB (47.06%) - LeetHub

---
+++ 3100-water-bottles-ii/3100-water-bottles-ii.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} numBottles
+ * @param {number} numExchange
+ * @return {number}
+ */
+var maxBottlesDrunk = function(numBottles, numExchange) {
+    let output = numBottles;
+    let empty = numBottles;
+    let leftover = 0
+
+    while (empty >= numExchange) {
+        let exchange = 0
+        while (empty >= numExchange) {
+            empty -= numExchange;
+            numExchange++;
+            exchange++
+        }
+        empty += exchange
+        output += exchange
+    }
+
+    return output
+};
