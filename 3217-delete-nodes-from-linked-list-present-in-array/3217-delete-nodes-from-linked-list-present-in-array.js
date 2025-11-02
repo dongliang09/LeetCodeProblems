11d1a68995cdd58d7a0b592d1d41a6012e80b778
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2025-11-01 22:53:18 -0700

    Time: 62 ms (88.61%), Space: 96 MB (100%) - LeetHub

--- 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.js
+++ 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.js
@@ -16,10 +16,15 @@ var modifiedList = function(nums, head) {
     let goodHead = null
     let lastGoodNode = null
     let count = 0;
+    let obj = {}
+
+    for (let i = 0; i < nums.length; i++) {
+        if (obj[nums[i]] === undefined) obj[nums[i]] = false 
+    }
 
     while (curr) {
         let currVal = curr.val
-        if (nums.indexOf(currVal) === -1) {
+        if (obj[currVal] === undefined) {
             if (goodHead === null) {
                 goodHead = curr;
                 lastGoodNode = curr;
