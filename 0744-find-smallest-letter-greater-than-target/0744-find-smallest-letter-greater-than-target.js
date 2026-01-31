2d83c818fc1a0bd6723936a9799f9ceb0837d909
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2026-01-31 10:54:51 -0800

    Time: 0 ms (100%), Space: 54.5 MB (86.27%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.js
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.js
@@ -5,18 +5,11 @@
  */
 var nextGreatestLetter = function(letters, target) {
     
-    // since it is already sorted, we just need to find the one greater than the target
+    if (letters[letters.length-1] < target) return letters[0]
+
     for (let i = 0; i < letters.length; i++) {
         if (letters[i] > target) return letters[i]
     }
-    
-    //return to first letter as default if nothing is greater than target
+
     return letters[0]
 };
-
-/*
-# Rank 161,072
-# Date 6/8/2023
-# Runtime: 66 ms, faster than 41.36%
-# Memory Usage: 43.4 MB, less than 87.87% 
-*/
