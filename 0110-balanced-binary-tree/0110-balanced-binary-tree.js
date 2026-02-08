025c7a0a5984efb49a74762f0ee0881c03c3a5b8
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2026-02-07 21:23:34 -0800

    Time: 144 ms (5.43%), Space: 47.3 MB (100%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+    
+    //================helper function ===========
+    function getHeight (rootNode) {
+      // Your code here
+      if (!rootNode) return -1;
+      if (rootNode.left === null && rootNode.right === null) return 0;
+
+      let numNodeLeft = getHeight(rootNode.left)
+      let numNodeRight = getHeight(rootNode.right)
+
+      if (numNodeLeft > numNodeRight) {
+        return numNodeLeft + 1
+      } else {
+        return numNodeRight + 1
+      }
+
+    };
+    //==================main logic
+    
+    let queue = [root];   
+    while (queue.length) {
+        let currNode = queue.shift();
+        if(currNode !== null) {
+            if (Math.abs(getHeight(currNode.left) - getHeight(currNode.right)) > 1) {
+              return false;
+            }
+        }
+        
+        if(currNode !== null && currNode.left) queue.push(currNode.left);
+        if(currNode !== null && currNode.right) queue.push(currNode.right);
+    }
+  return true;
+    
+};
