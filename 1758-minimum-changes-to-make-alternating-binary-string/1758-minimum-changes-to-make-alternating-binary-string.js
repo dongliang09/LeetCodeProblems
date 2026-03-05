f9eb2617df762e79331fe58d3be27d9c3847dd96
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2026-03-05 00:52:56 -0800

    Time: 5 ms (13.04%), Space: 54.6 MB (39.13%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.js
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.js
@@ -3,65 +3,27 @@
  * @return {number}
  */
 var minOperations = function(s) {
-    
-    // ================ idea 2 ==============
-    // make two alternating string with 0 and 1, one starts with 0, and another starts with 1
-    // compare the the s, see which one has lower number of opeartions
-    
-    let str0 = "0";
-    let str1 = "1";
-    let diff0 = 0;
-    let diff1 = 0;
-    
-    //construct a string of same length with original
-    for (let i = 1; i < s.length; i++) {
-        if (str0[i-1] === "0") str0 += "1"
-        else str0 += "0"
-        
-        if (str1[i-1] === "0") str1 += "1"
-        else str1 += "0"
-    }
-    
-    // check the diff of string with original
+
+    let count1 = 0;
+    let count2 = 0;
+    let out = s.length;
+
+    // start with 0
     for (let i = 0; i < s.length; i++) {
-        if (str0[i] != s[i]) diff0++;
-        if (str1[i] != s[i]) diff1++;
+        let curr = s[i]
+        if ( (i % 2 === 0) && (curr !== '0')) count1++
+        else if ( (i % 2 === 1) && (curr !== '1')) count1++
     }
-    
-    return diff0 > diff1 ? diff1 : diff0;
-    
 
+    // start with 1
+    for (let i = 0; i < s.length; i++) {
+        let curr = s[i]
+        if ( (i % 2 === 0) && (curr !== '1')) count2++
+        else if (( i % 2 === 1) && (curr !== '0')) count2++
+    }
+
+    if (count1 < out) out = count1
+    if (count2 < out) out = count2
+
+    return out
 };
- 
-
-/*
-
-Rank: 121,540
-Date: 12/23/2023
-Runtime: 113 ms, faster than 5.17% 
-Memory Usage: 48.7 MB, less than 5.17% 
-
-============= test case  =========
-
-"110010"
-"1111"
-"0010"
-"0100"
-
-============= first try ==============
-    let count = 0;
-    let tempCount = 1;
-    
-    for (let i = 1; i < s.length; i++) {
-        if (s[i] === s[i-1]) {
-            
-            if(tempCount === 1) {
-                count++;
-                tempCount = 0;
-            }
-            tempCount++;
-        }
-    }
-    
-    return count
-*/
