a9474f44cbf80f491be1ae98be59a75ba5a745e7
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2026-03-06 00:54:30 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.js
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.js
@@ -7,6 +7,7 @@ var checkOnesSegment = function(s) {
     let count = 0;
     let lastZeroIndex = -1
 
+    // increase count when we have zero and previous characater is one
     for (let i = 0; i < s.length; i++) {
         let curr = s[i];
         if (curr === '0' && s[i-1] === '1') {
@@ -15,6 +16,7 @@ var checkOnesSegment = function(s) {
         }
     }
 
+    // check the last continguous segment of one
     if (s[s.length-1] === '1' && s.length - 1 > lastZeroIndex && lastZeroIndex !== -1) count++
 
     if (count <= 1) return true
