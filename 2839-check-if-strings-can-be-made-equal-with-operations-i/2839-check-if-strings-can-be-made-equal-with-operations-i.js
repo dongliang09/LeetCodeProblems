6cb4086c2a800749ffabf1d28ce004fbf9da109e
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2026-03-31 21:33:12 -0700

    Time: 4 ms (9.57%), Space: 57.4 MB (36.93%) - LeetHub

---
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var canBeEqual = function(s1, s2) {
+    
+    let s1a = "", s1b ="", s2a = "", s2b = ""
+
+    for (let i = 0; i < s1.length; i++) {
+        if (i % 2 === 0) {
+            s1a += s1[i];
+            s2a += s2[i];
+        } else {
+            s1b += s1[i];
+            s2b += s2[i];
+        }
+    }
+
+    s1a = s1a.split('').sort().join('');
+    s1b = s1b.split('').sort().join('');
+    s2a = s2a.split('').sort().join('');
+    s2b = s2b.split('').sort().join('');
+
+    if (s1a === s2a && s1b === s2b) return true
+    else return false
+};
