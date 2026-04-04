b9b56f67a113b45682ea31bf7131ef8f06b48836
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2026-04-04 10:33:36 -0700

    Time: 200 ms (6.67%), Space: 95.9 MB (80%) - LeetHub

---
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} encodedText
+ * @param {number} rows
+ * @return {string}
+ */
+var decodeCiphertext = function(encodedText, rows) {
+    if (rows === 1) return encodedText
+
+    let originText = "";
+    let currCol = 0;
+    let columns = encodedText.length / rows;
+    /*
+        (1) number of empty space is the same as current row number, 
+            ie, column starting index should be same as row index which  
+        (2) we are starting with some index of each row, 
+            this index only increment when we finsihed every row
+        (3) when we finish the last column of last row, 
+            ie, that mapping won't exist in originText, then we skip
+        (4) how we know we finished? 
+            2 consecutive spaces, we need to record the last character
+    */
+    for (let col = 0; col <= columns; col++) {
+        for (let row = 0; row < rows; row++) {
+            let emptySpace = row
+            let pos = currCol + row*columns + emptySpace
+            let currChar = encodedText[pos]
+            if (pos < encodedText.length) originText += currChar
+        }
+        currCol++;
+    }  
+    
+    // remove extra empty space if any
+    return originText.trimEnd();
+    
+
+
+};
