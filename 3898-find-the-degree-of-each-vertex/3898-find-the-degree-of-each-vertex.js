4e02dc42c57b218379d06a1f7cdc8c04d2fc1e72
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2026-04-13 20:54:19 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.js
+++ 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.js
@@ -3,8 +3,10 @@
  * @return {number[]}
  */
 var findDegrees = function(matrix) {
+
     let output = []
 
+    // sum of each row is the degree
     for (let i = 0; i < matrix.length; i++) {
         let sum = 0;
         for (let j = 0; j < matrix[i].length; j++) {
