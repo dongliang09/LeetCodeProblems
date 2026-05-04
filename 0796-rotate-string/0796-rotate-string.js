c0b23f37253c2d2b2698facfbe2920fd34fa8527
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2026-05-03 17:02:00 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.js
+++ 0796-rotate-string/0796-rotate-string.js
@@ -4,21 +4,22 @@
  * @return {boolean}
  */
 var rotateString = function(s, goal) {
-    
-    
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] === goal[0]) {
-            let currStr = s.substring(i) + s.substring(0,i)
-            if (currStr === goal) return true
-        }
+
+    // find all possible index of the starting letter from goal in the original string
+    // then try until we find a match 
+
+    // can also try more than one letter, worst case is one of them is head or tail
+
+    let arr = []
+    for (let i = 0; i <s.length; i++) {
+        if (s[i] === goal[0]) arr.push(i)
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        let front = s.slice(arr[i])
+        let back = s.slice(0, arr[i])
+        if (front+ back === goal) return true 
     }
     
     return false
 };
-
-/*
-Date: 11/6/2024
-Rank: 113,163
-Runtime: 0 ms, faster than 100.00% 
-Memory Usage: 48.4 MB, less than 93.87%
-*/
