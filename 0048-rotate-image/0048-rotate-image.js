a12e4c490422fcacc3b7471266acd9d49afd8001
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2026-05-03 20:52:24 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.js
+++ 0048-rotate-image/0048-rotate-image.js
@@ -3,6 +3,7 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function(matrix) {
+    
     const n = matrix.length;
     const oddOrEven = n % 2;
     const centerPointI = Math.floor((n - 1) / 2) + 1;
@@ -20,4 +21,26 @@ var rotate = function(matrix) {
             matrix[j][n - i - 1] = temp;//4th to 1st
         }
     }
+    
+    
 };
+/*
+ n  max(i), max(j)
+ 2  0   0
+ 3  1   0
+ 4  1   1
+ 5  2   1
+ 6  2   2
+ 7  3   2
+ 
+ 
+*/
+/* test case
+[[1,2,3, 4, 5, 6,7],[8,9,10,11,12,13,14], [15,16,17,18,19,20,21],[22,23,24,25,26,27,28], [29,30,31,32,33,34,35],[36,37,38,39,40,41,42], [43,44,45,46,47,48,49]]
+
+
+[[1,2,3],[4,5,6],[7,8,9]]
+
+[[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+[[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+*/
