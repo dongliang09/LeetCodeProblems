8fc7eb2bc994975f7dced08632f47fe448b88270
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2026-05-06 00:13:47 -0700

    Time: 24 ms (52.63%), Space: 83.6 MB (52.63%) - LeetHub

---
+++ 1861-rotating-the-box/1861-rotating-the-box.js
@@ -0,0 +1,54 @@
+/**
+ * @param {character[][]} boxGrid
+ * @return {character[][]}
+ */
+var rotateTheBox = function(boxGrid) {
+
+    /**
+        anything on the left of obstacle will fall on it, 
+        i.e., find the number of stones between each obstacles in one row
+
+        when we detects an obstacle, or we hit the end of the row, 
+        get the count of stone and empty to fill on the top of obstacle in new array
+
+        do the same for each row in old array
+     */
+
+    let arr = Array.from({ length: boxGrid[0].length }, () => Array(boxGrid.length).fill("."));
+
+    for(let i = 0; i < boxGrid.length; i++) {
+        let stoneCount = 0;
+        for (let j = 0; j < boxGrid[0].length; j++) {
+            let curr = boxGrid[i][j]
+
+            // console.log("curr",curr)
+
+            if (curr === '#') stoneCount++
+            else if (curr === '*'){
+                arr[j][boxGrid.length - i - 1] = '*'
+                // console.log("obstacle, stone count", stoneCount)
+
+                // fill the stone
+                for (let k = 0; k < stoneCount; k++) {
+                    arr[j - k - 1][boxGrid.length - 1- i] = "#"
+                    // console.log('stone pos', [j - k - 1, boxGrid.length- 1 - i])
+                }
+                stoneCount = 0;
+            } 
+            if (j === boxGrid[0].length - 1){
+
+                // console.log("end of row, stone count", stoneCount)
+                // fill the stone
+                for (let k = 0; k < stoneCount; k++) {
+                    arr[j - k][boxGrid.length - 1-i] = "#"
+                    // console.log('stone pos', [j - k, boxGrid.length- 1-i])
+                }
+                stoneCount = 0;
+            } 
+
+
+        }
+    }
+    return arr
+    
+};
