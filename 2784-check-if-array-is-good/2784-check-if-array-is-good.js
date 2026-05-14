cea372b52f54572f99d412e31c261f39080d0913
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2026-05-13 18:32:24 -0700

    Time: 1 ms (72.92%), Space: 55.9 MB (75%) - LeetHub

---
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isGood = function(nums) {
+    const countObj = {}
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] >= nums.length) return false
+        if (nums[i] in countObj) {
+            if (nums[i] in countObj && nums[i] !== nums.length - 1) return false
+            if (countObj[nums[i]] === 1) countObj[nums[i]]++
+            else return false
+        }
+        
+        else countObj[nums[i]] = 1
+    }
+    
+    return true
+};
