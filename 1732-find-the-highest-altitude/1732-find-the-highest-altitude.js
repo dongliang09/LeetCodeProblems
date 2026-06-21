cb402f779782c88b88c32c5300ff0e9b0fc46662
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2026-06-20 23:00:31 -0700

    Time: 1 ms (23.34%), Space: 54.1 MB (26.59%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.js
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.js
@@ -3,27 +3,21 @@
  * @return {number}
  */
 var largestAltitude = function(gain) {
-    
-    // if we want to get the answer with one loop
-    // we need to track the highest altitude and the current height
-    // if current height is bigger than the highest altitude in record, replace it
-    
-    let max = 0
-    let sum = 0
-    
+
+    let altitude = []
+    let currHeight = 0
+    let output = 0
+
     for (let i = 0; i < gain.length; i++) {
-        sum += gain[i]
-        if (sum > max) max = sum
+        currHeight += gain[i]
+        altitude.push(currHeight)
     }
+
+    for (let i = 0; i < altitude.length; i++) {
+        let curr = altitude[i]
+        if (curr > output) output = curr
+    }
+
+    return output
     
-    return max
 };
-
-
-/*
-# Rank 162,270
-# Date 6/5/2023
-# Runtime: 62 ms, faster than 35.89%
-# Memory Usage: 42.3 MB, less than 18.08% 
-
-*/
