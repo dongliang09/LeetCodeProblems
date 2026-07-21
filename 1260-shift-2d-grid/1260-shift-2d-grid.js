bb3ead165607a13340dcc5378d1775f37fc86968
Dongliang Li <67977179+dongliang09@users.noreply.github.com>
2026-07-20 18:02:09 -0700

    Time: 17 ms (9.38%), Space: 62.6 MB (15.63%) - LeetHub

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[][]} grid
+ * @param {number} k
+ * @return {number[][]}
+ */
+var shiftGrid = function(grid, k) {
+
+    let output = []
+
+    let arr = []
+    let count
+
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            arr.push(grid[i][j])
+        }
+    }
+
+    start = arr.length - k % arr.length
+    arr = arr.slice(start).concat(arr.slice(0,start))
+    console.log(arr)
+
+    for (let i = 0; i < arr.length; i+=0) {
+        let row = []
+        for (let j = 0; j < grid[0].length; j++) {
+            row.push(arr[i])
+            i++;
+        }
+        output.push(row)
+    }
+
+    return output
+    
+    
+};
